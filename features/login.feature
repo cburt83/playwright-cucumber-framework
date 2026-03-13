f18c31bfdf4cc87c8a91e9e55463e18021192e71
Chris <chris.j.burt@hotmail.com>
2026-03-13 11:27:19 +0000

    Fixed Formatting of Files

--- features/login.feature
+++ features/login.feature
@@ -5,7 +5,7 @@ Feature: Login
     When I login with valid credentials
     Then I should be redirected to the products page
 
-Scenario: Unsuccessful login with invalid credentials
+  Scenario: Unsuccessful login with invalid credentials
     Given I am on the login page
     When I login with invalid credentials
     Then I should see the error message "Epic sadface: Username and password do not match any user in this service"
--- features/step-definitions/product.steps.ts
+++ features/step-definitions/product.steps.ts
@@ -6,9 +6,9 @@ import { ProductsPage } from '../../src/pages/ProductsPage';
 When('I add the first product to the cart', async function (this: CustomWorld) {
     this.ProductsPage = new ProductsPage(this.page);
     await this.ProductsPage.addFirstProductToCart();
-         });
+});
 
-Then('the cart badge should show {string}', async function  (this: CustomWorld, string) {
-            await this.ProductsPage.ProductInBasket(string);
-         });
+Then('the cart badge should show {string}', async function (this: CustomWorld, string) {
+    await this.ProductsPage.ProductInBasket(string);
+});
 
